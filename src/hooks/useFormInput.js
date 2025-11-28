88f1273e91bf6a86642cbdf77dc5e25574e0658e
Kamal Habibi <habibi.kamal@outlook.com>
2025-11-28 14:29:21 +0330

    add init

---
+++ src/hooks/useFormInput.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export default function useFormInput(initialValue){
+    const[value,setValue]=useState(initialValue);
+
+    const onChangeHandler=(event)=>{
+        setValue(event.target.value);
+    }
+
+    const resetValue=()=>{
+        setValue('')
+    }
+    return{
+        value,
+        resetValue,
+        onChange:onChangeHandler
+    }
+}
